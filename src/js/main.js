e9ef583ab492e7222f26cdd0cdf6de6efe7895ac
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-01-25 18:36:31 +0200

    список

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
+import fetchApi from '../services/apiService';
+import showMovie from './showMovieList';
 
+function getMovie() {
+  fetchApi.fetchTrending().then(({ results }) => showMovie(results));
+}
+
+getMovie();
