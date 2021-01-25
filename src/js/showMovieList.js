53f6ad24857db933dd498bd447ad9a22a1315382
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-01-25 22:12:44 +0200

    хандлебар хелпер

--- src/js/showMovieList.js
+++ src/js/showMovieList.js
@@ -1,5 +1,6 @@
 import movieMarkup from '../templates/movieMarkup.hbs';
 import showMovieCard from './showMovieCard';
+// import handlebars from 'handlebars';
 
 const refs = {
   listMovies: document.querySelector('.list_movies'),
@@ -10,12 +11,20 @@ refs.listMovies.addEventListener('click', onCardClick);
 function onCardClick(e) {
   e.preventDefault();
 
+  if (e.target.dataset.id === undefined) {
+    return;
+  }
+
   if (e.target.nodeName === 'BUTTON') {
     return;
   }
   //   console.dir(e.target.nodeName);
   showMovieCard(e.target.dataset.id);
 }
+
+// handlebars.registerHelper('stringifyFunc', function (fn) {
+//   return fn;
+// });
 
 export default function showMovie(data) {
   refs.listMovies.insertAdjacentHTML('beforeend', movieMarkup(data));
