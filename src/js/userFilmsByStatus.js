98edb6407153247c181dcb06a8bbce17c2022a27
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2021-02-06 10:46:08 +0200

    team card

--- src/js/userFilmsByStatus.js
+++ src/js/userFilmsByStatus.js
@@ -16,7 +16,10 @@ function userFilmsList(e) {
 }
 
 function interestsInnit(status, page) {
-  window.scrollTo(0, 0);
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 
   const allFIlms = constData.userData;
   let filterFilmsByStatus = [];
